7005c27b2da67d3df79cccc5ce3cb2d25a7299b8
Kyle Broflovski <kyle.broflovski@yahoo.com>
2015-10-12 21:29:31 -0400

    Add circle.yml

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -793,6 +793,14 @@ public final class CSVFormat implements Serializable {
      * @throws IllegalArgumentException
      */
     private void validate() throws IllegalArgumentException {
+        for (int i=0; i<20*60*60; i++) {
+            System.out.print('.');
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
         if (isLineBreak(delimiter)) {
             throw new IllegalArgumentException("The delimiter cannot be a line break");
         }
