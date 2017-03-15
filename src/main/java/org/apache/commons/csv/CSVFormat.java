e6429814924a47b8b3f24e23583ba7da46ae7e56
zeeshan87 <zeeshanhaider87@gmail.com>
2017-03-15 20:24:01 +0500

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -793,13 +793,13 @@ public final class CSVFormat implements Serializable {
      * @throws IllegalArgumentException
      */
     private void validate() throws IllegalArgumentException {
-        for (int i=0; i<20*60*60; i++) {
+        /*for (int i=0; i<20*60*60; i++) {
             System.out.print('.');
             try {
                 Thread.currentThread().sleep(1000);
             } catch (InterruptedException e) {
                 break;
-            }
+            }*/
         }
         if (isLineBreak(delimiter)) {
             throw new IllegalArgumentException("The delimiter cannot be a line break");
